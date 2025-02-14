173aa07522a5e93b1ffc3bd619d09c923cc0cb7b
Mark Peters <mapeters101@earthlink.net>
2025-02-13 22:31:35 -0500

    Work on lesson011 from 1:00 to 3:05.

--- lesson011.py
+++ lesson011.py
@@ -2,6 +2,16 @@ if __name__ == '__main__':
     fruits = ["apple", "banana", "cherry"]
     for fruit in fruits:
         print(fruit)
+    print('-'*50)
+
+    for i in range(len(fruits)):
+        print(i, fruits[i])
+    print('-'*50)
+
+    for i, fruit  in enumerate(fruits):
+        print(i, fruits[i])
+    print('-'*50)
+
 
 
 
