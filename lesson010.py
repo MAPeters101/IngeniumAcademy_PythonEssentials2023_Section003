8983020217db02a5c0578e320e1ecf44dd790d4d
Mark Peters <mapeters101@earthlink.net>
2025-02-13 22:20:26 -0500

    Work on lesson010 to 2:00.

---
+++ lesson010.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    length = 800
+
+    if length < 500:
+        print("small")
+    elif 500 <= length < 1000:
+        print("medium")
+    elif 1000 <= length < 2000:
+        print("large")
+    else:
+        print("extra large")
+
+
+
