af7d0d43ad18b7dfd11893b17602fce7937150b8
Mark Peters <mapeters101@earthlink.net>
2025-02-13 22:24:47 -0500

    Complete work on lesson010 from 2:00.

--- lesson010.py
+++ lesson010.py
@@ -10,5 +10,15 @@ if __name__ == '__main__':
     else:
         print("extra large")
 
+    print("-"*30)
+    is_premium_member = True
+    days_registered = 400
+    special_access_code = False
+
+    if (is_premium_member and days_registered >365) or special_access_code:
+        print("Access to the premium feature granted!")
+    else:
+        print("Access denied.")
+
 
 
